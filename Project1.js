1818873c931b3f2a82c8803b5f5bc790320ccac3
td89451 <delaneyt@student.ncmich.edu>
2017-12-30 16:53:56 -0500

    Added jsdoc documentation and changed prompt wording.

--- Project1.js
+++ Project1.js
@@ -10,6 +10,11 @@ const PROMPT = require('readline-sync');
 let lotNumber, bedroomCount, bathroomCount, garageSize, total;
 const BASE_PRICE = 50000, BEDROOM_PRICE = 17000, BATHROOM_PRICE = 12500, PER_CAR_PRICE = 6000;
 
+/**
+ * @method
+ * @desc The dispatch method
+ * @returns {null}
+ */
 function main() {
     setLotNumber();
     setBedroomCount();
@@ -21,24 +26,56 @@ function main() {
 
 main();
 
+/**
+ * @method
+ * @desc Set the lot number
+ * @returns {null}
+ */
 function setLotNumber() {
     lotNumber = PROMPT.question(`\nPlease enter your lot number: `);
 }
 
+/**
+ * @method
+ * @desc Set the amount of bedrooms
+ * @returns {null}
+ */
 function setBedroomCount() {
     bedroomCount = PROMPT.question(`\nPlease enter desired number of bedrooms: `);
 }
 
+/**
+ * @method
+ * @desc Set the amount of bathrooms
+ * @returns {null}
+ */
 function setBathroomCount() {
     bathroomCount = PROMPT.question(`\nPlease enter desired number of bathrooms: `);
 }
+
+/**
+ * @method
+ * @desc Set the size of the garage
+ * @returns {null}
+ */
 function setGarageSize() {
-    garageSize = PROMPT.question('\nPlease enter desired size of garage: ');
+    garageSize = PROMPT.question('\nPlease enter desired size of garage(in cars): ');
 }
+
+/**
+ * @method
+ * @desc Calculate the final price of building the house
+ * @returns {null}
+ */
 function calcFinalPrice () {
     total = BASE_PRICE + BEDROOM_PRICE*bedroomCount + BATHROOM_PRICE*bathroomCount + PER_CAR_PRICE*garageSize;
 }
 
+/**
+ * @method
+ * @desc Output the final price of building the house
+ * @returns {null}
+ */
 function printResults() {
     process.stdout.write('\x1Bc'); //Clears the screen
     console.log(`\nIt will cost \$${total} to build your house at lot number ${lotNumber}.`);
